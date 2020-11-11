3c886ab25b639ad1c694a16ef2dfbda9b644d5eb
Fayne Aldan <FayneAldan@gmail.com>
2020-11-10 19:28:26 -0700

    Switch to folders and RobLoach's datfile library

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const glob = require('glob');
+const fs = require("fs");
+const chalk = require("chalk");
+const glob = require("glob");
 
 // Set to true to only show bad / missing BIOS files.
-const onlyBad = process.argv.map(v => v.toLowerCase()).includes('onlybad');
+const onlyBad = process.argv.map((v) => v.toLowerCase()).includes("onlybad");
 
 let name = chalk.gray.bold;
 let comment = chalk.gray.italic;
@@ -12,82 +11,88 @@ let ok = chalk.green;
 let fail = chalk.bold.red.bgWhite;
 let unknown = chalk.gray.strikethrough;
 
-if (!fs.existsSync('data.json')) {
-	console.log('data.json not found!');
-	console.log('Use "node update" to download it automatically.');
-	return;
+if (!fs.existsSync("data.json")) {
+  console.log("data.json not found!");
+  console.log('Use "node update" to download it automatically.');
+  return;
 }
 
-if (!String.prototype.padStart)
-	String.prototype.padStart = require('./lib/padStart.js');
+if (!chalk.supportsColor) ok = unknown = () => "";
+if (!fs.existsSync("system")) fs.mkdirSync("system");
 
-const system = require('./lib/system.js');
-if (!chalk.supportsColor) ok = unknown = () => '';
-if (!system && !fs.existsSync('system')) fs.mkdirSync('system');
-
-let results = require('./lib/results.js')(require('./data.json'));
-let longestName = 0, anyIncap = false;
+let results = require("./lib/results.js")(require("./data.json"));
+let longestName = 0;
+let anyIncap = false;
 for (sys in results)
-	for (rom of results[sys])
-		if (rom instanceof Object) {
-			longestName = Math.max(longestName, rom.name.length);
-			if (rom.incap) anyIncap = true;
-		}
+  for (rom of results[sys])
+    if (rom instanceof Object) {
+      longestName = Math.max(longestName, rom.name.length);
+      if (rom.incap) anyIncap = true;
+    }
 longestName += 1;
 
 let ignore = [];
 if (onlyBad) {
-	let newr = {};
-	for (sys in results) {
-		let news = [], any = false;
-		for (rom of results[sys])
-			if (rom instanceof Object) {
-				if (!rom.check || rom.incap) {
-					news.push(rom);
-					any = true;
-				} else ignore.push(rom.file);
-			} else news.push(rom);
-		if (any) newr[sys] = news;
-	}
-	results = newr;
+  let newr = {};
+  for (sys in results) {
+    let news = [],
+      any = false;
+    for (rom of results[sys])
+      if (rom instanceof Object) {
+        if (!rom.check || rom.incap) {
+          news.push(rom);
+          any = true;
+        } else ignore.push(rom.file);
+      } else news.push(rom);
+    if (any) newr[sys] = news;
+  }
+  results = newr;
 }
 
 for (sys in results) {
-	console.log();
-	console.log(name(sys));
-	for (rom of results[sys]) {
-		if (rom instanceof Object) {
-			ignore.push(rom.file || rom.name);
-			if (rom.missing)
-				console.log(fail(rom.name.padStart(longestName)), 'Missing');
-			else if (rom.error)
-				console.log(fail(rom.name.padStart(longestName)), rom.error);
-			else {
-				console.log(
-					rom.name.padStart(longestName),
-					cc(rom.size)('Size'), cc(rom.crc)('CRC'),
-					cc(rom.md5)('MD5'), cc(rom.sha1)('SHA1'),
-					rom.incap ? fail('INCAP') : ''
-				);
-			}
-		} else console.log(comment(rom));
-	}
+  console.log();
+  console.log(name(sys));
+  for (rom of results[sys]) {
+    if (rom instanceof Object) {
+      ignore.push(rom.file || rom.name);
+      if (rom.missing)
+        console.log(fail(rom.name.padStart(longestName)), "Missing");
+      else if (rom.error)
+        console.log(fail(rom.name.padStart(longestName)), rom.error);
+      else {
+        console.log(
+          rom.name.padStart(longestName),
+          cc(rom.size)("Size"),
+          cc(rom.crc)("CRC"),
+          cc(rom.md5)("MD5"),
+          cc(rom.sha1)("SHA1"),
+          rom.incap ? fail("INCAP") : ""
+        );
+      }
+    } else console.log(comment(rom));
+  }
 }
 
 if (anyIncap) {
-	console.log();
-	console.log(fail('INCAP') + ' indicates a BIOS with INcorrect CAPitalization.');
-	console.log('On Linux, this will prevent RetroArch from finding the file.');
-	console.log('This has no effect on Windows and (standard) Mac filesystems.');
+  console.log();
+  console.log(
+    fail("INCAP") + " indicates a BIOS with INcorrect CAPitalization."
+  );
+  console.log("On Linux, this will prevent RetroArch from finding the file.");
+  console.log("This has no effect on Windows and (standard) Mac filesystems.");
 }
-let extras = glob.sync('*', { ignore: ignore, cwd: system || 'system', nodir: true });
+let extras = glob.sync("*", {
+  ignore: ignore,
+  cwd: "system",
+  nodir: true,
+});
 if (extras.length) {
-	console.log();
-	console.log(name('Stray files'));
-	for (extra of extras) console.log(extra);
+  console.log();
+  console.log(name("Stray files"));
+  for (extra of extras) console.log(extra);
 }
 function cc(check) {
-	if (check ===  true) return ok;
-	if (check === false) return fail;
-	return unknown;
+  if (check === true) return ok;
+  if (check === false) return fail;
+  return unknown;
 }
--- update.js
+++ update.js
@@ -1,55 +1,49 @@
-const https = require('https');
-const fs = require('fs');
-const cp = require('cli-command-parser');
-const url = 'https://raw.githubusercontent.com/libretro/libretro-database/master/dat/System.dat';
+const util = require("util");
+const datfile = require("robloach-datfile");
+const https = require("https");
+const fs = require("fs");
+const stream = require("stream");
 
-(new Promise((resolve, reject) => {
-	https.get(url, res => {
-		if (res.statusCode !== 200) {
-			reject(res.statusCode + ' ' + res.statusMessage);
-			res.resume();
-			return;
-		}
-		res.setEncoding('utf8');
-		let data = '';
-		res.on('data', chunk => { data += chunk; });
-		res.on('end', () => { resolve(data); });
-	}).on('error', e => {
-		reject(e.message);
-	});
-})).then(data => new Promise(resolve => {
-	let bios = data.split('\n');
-	bios.splice(0,10);
-	bios = bios.filter(Boolean).filter(v => v != 'game (').filter(v => v != ')');
-	let out = {};
-	let name = "";
-	for (v of bios) {
-		if (v.startsWith('\tcomment ')) {
-			name = v.substring(10, v.length-1);
-			if (!out[name] && name != 'System')
-				out[name] = [];
-        } else if (v.startsWith('\trom ')) {
-			var obj = {};
-			v = cp(v.substring(7, v.length-2));
-			for (let i = 0; i < v.length; i += 2)
-				obj[v[i]] = v[i+1];
-			if (obj.size) obj.size = Number(obj.size);
-			if (obj.crc) obj.crc = obj.crc.toLowerCase();
-			if (obj.md5) obj.md5 = obj.md5.toLowerCase();
-			if (obj.sha1) obj.sha1 = obj.sha1.toLowerCase();
-			if (obj.name) out[name].push(obj);
-		}
-	}
-	if (out[0] == '') out.splice(0,1);
-	resolve(out);
-})).then(data => new Promise((resolve, reject) => {
-	fs.writeFile('data.json', JSON.stringify(data), err => {
-		if (err) reject(err);
-		resolve();
-	});
-})).then(() => {
-	console.log('Successfully updated BIOS database!');
-}).catch(e => {
-	console.error('Failed to update BIOS database.');
-	console.error(e);
-});
+const url =
+  "https://raw.githubusercontent.com/libretro/libretro-database/master/dat/System.dat";
+const dest = "System.dat";
+
+new Promise((resolve, reject) => {
+  let data = "";
+  https
+    .get(url, (res) => {
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve(data);
+      });
+    })
+    .on("error", (err) => {
+      reject(err.message);
+    });
+})
+  .then(async (data) => {
+    data = data.substring(data.indexOf("game ("));
+    data = data.replace(/.* "System"$/gm, "");
+    data = data.replace(/^\tcomment /gm, ")\ngame (\n\tname ");
+    return data;
+  })
+  .then(async (data) => {
+    const input = new stream.Readable();
+    input.push(data);
+    input.push(null);
+    return input;
+  })
+  .then((data) => datfile.parse(data))
+  .then((data) =>
+    util.promisify(fs.writeFile)("data.json", JSON.stringify(data))
+  )
+  .then(() => {
+    console.log("Successfully updated BIOS database!");
+  })
+  .catch((e) => {
+    console.log("Failed to update BIOS database.");
+    console.error(e);
+  });
